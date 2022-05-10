c1e949ac8acd3bd123eb8e1aa2482279dd80ec06
sameh sayed <samehsayed628@gmail.com>
2022-05-10 22:18:53 +0200

    some updates

---
+++ src/components/content/market-updates.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="market-updates row">
+    <div class="col-md-4 market-update-gd">
+      <div class="market-update-block clr-block-1 row">
+        <div class="col-md-8 market-update-left">
+          <h3>83</h3>
+          <h4>Registered User</h4>
+          <p>Other hand, we denounce</p>
+        </div>
+        <div class="col-md-4 market-update-right">
+          <i class="fa fa-file-text-o"> </i>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+    <div class="col-md-4 market-update-gd">
+      <div class="market-update-block clr-block-2 row">
+        <div class="col-md-8 market-update-left">
+          <h3>135</h3>
+          <h4>Daily Visitors</h4>
+          <p>Other hand, we denounce</p>
+        </div>
+        <div class="col-md-4 market-update-right">
+          <i class="fa fa-eye"> </i>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+    <div class="col-md-4 market-update-gd">
+      <div class="market-update-block clr-block-3 row">
+        <div class="col-md-8 market-update-left">
+          <h3>23</h3>
+          <h4>New Messages</h4>
+          <p>Other hand, we denounce</p>
+        </div>
+        <div class="col-md-4 market-update-right">
+          <i class="fa fa-envelope-o"> </i>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+    <div class="clearfix"></div>
+  </div>
+</template>
---
+++ src/components/content/recent-followers.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="work-progres">
+    <div class="chit-chat-heading">Recent Followers</div>
+    <div class="table-responsive">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Project</th>
+            <th>Manager</th>
+
+            <th>Status</th>
+            <th>Progress</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Face book</td>
+            <td>Malorum</td>
+
+            <td>
+              <span class="label label-danger">in progress</span>
+            </td>
+            <td><span class="badge badge-info">50%</span></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Twitter</td>
+            <td>Evan</td>
+
+            <td>
+              <span class="label label-success">completed</span>
+            </td>
+            <td><span class="badge badge-success">100%</span></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Google</td>
+            <td>John</td>
+
+            <td>
+              <span class="label label-warning">in progress</span>
+            </td>
+            <td><span class="badge badge-warning">75%</span></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>LinkedIn</td>
+            <td>Danial</td>
+
+            <td>
+              <span class="label label-info">in progress</span>
+            </td>
+            <td><span class="badge badge-info">65%</span></td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Tumblr</td>
+            <td>David</td>
+
+            <td>
+              <span class="label label-warning">in progress</span>
+            </td>
+            <td><span class="badge badge-danger">95%</span></td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>Tesla</td>
+            <td>Mickey</td>
+
+            <td>
+              <span class="label label-info">in progress</span>
+            </td>
+            <td><span class="badge badge-success">95%</span></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
